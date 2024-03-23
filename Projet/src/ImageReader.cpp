4149f7eea7a91f01eeed350094639216ec663f85
Kevin QUACH <kevinquach93270@gmail.com>
2024-03-24 00:57:29 +0100

    Added connected components detector
    Improved the noise removal.
    The program now analyzes CCs.
    It will frame the ones whose area are big enough (person).
    There are still part of the background that gets captured.
    Upon changing scene, generate a new BGSubtractor.

--- Projet/src/ImageReader.cpp
+++ Projet/src/ImageReader.cpp
@@ -32,9 +32,19 @@ int main()
 
     std::regex pattern("dataset");
     Mat fgMask;
+    Mat element = getStructuringElement(MORPH_RECT, Size(3, 3));
+    fs::path current_parent_path = "./";
+    
     for (const auto filepath : sorted_by_name){
-        // std::filesystem::path path = file_entry.path();
-        // std::cout << path << std::endl;
+
+        // Checking if it's still the same scene, if not, create a new BGSubtractor
+        fs::path parent_path = filepath.parent_path();
+        if (current_parent_path.compare(parent_path) != 0){
+            std::cout << filepath.parent_path() << std::endl;
+            current_parent_path = parent_path;
+            pBackSub = createBackgroundSubtractorMOG2();
+        }
+
 
         std::string image_path = samples::findFile(filepath);
         Mat img = imread(image_path, IMREAD_GRAYSCALE);
@@ -46,6 +56,28 @@ int main()
         Mat dst;
         equalizeHist(img, dst);
         pBackSub->apply(dst, fgMask);
+
+        // ------- Removing noise with morphological transformation -------
+
+        morphologyEx(fgMask, fgMask, MORPH_ERODE, element, Point(-1, -1), 5); 
+        morphologyEx(fgMask, fgMask, MORPH_DILATE, element, Point(-1, -1), 8); 
+
+        // ------- Looking for ppl among connected components -------
+
+        Mat labels, stats, centroids;
+        int nbcomp = connectedComponentsWithStats(fgMask, labels, stats, centroids, 4, CV_32S);
+        for (int i = 1; i < nbcomp; i++){  // Start at 1 because 0 is the background.
+            int area = stats.at<int>(i, CC_STAT_AREA);
+            if (area > 2000){
+                Point pt1 = Point(stats.at<int>(i, CC_STAT_LEFT), stats.at<int>(i, CC_STAT_TOP));
+                Point pt2 = Point(pt1.x + stats.at<int>(i, CC_STAT_WIDTH), pt1.y + stats.at<int>(i, CC_STAT_HEIGHT));
+                // Scalar color = Scalar(255, 255, 0);
+                rectangle(fgMask, pt1, pt2, 255);
+            }
+        }
+
+        // ----------------------------------------------------------
+
         std::string filename = filepath.filename();
         imshow("Display window", fgMask);
         int k = waitKey(0); // Wait for a keystroke in the window
